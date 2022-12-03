075ed4042702947f95cbbdb96fc1b38c49d51a65
chaminju927 <minjoo92718@gmail.com>
2022-12-04 02:41:05 +0900

    .

--- tictactoe/tictactoe1.js
+++ tictactoe/tictactoe1.js
@@ -2,20 +2,18 @@ const tr1 = document.getElementById('tr1');
 const tr2 = document.getElementById('tr2');
 const tr3 = document.getElementById('tr3');
 const trs = [tr1, tr2, tr3];
-const tds =[];
+const tds = [];
 let turn = 'X';
 
 const marking = function(event) {
-    const trNumber =
-trs.indexOf(event.target.parentNode);
+    const trNumber = trs.indexOf(event.target.parentNode);
     console.log('trNumber', trNumber);
-    const tdNumber =
-tds[trNumber].indexOf(event.target);
-   console.log('tdNumber', tdNumber);
+    const tdNumber = tds[trNumber].indexOf(event.target);
+    console.log('tdNumber', tdNumber);
 
-   if (tds[trNumber][tdNumber].textContent !==
-    '') {  console.log('Empty');
-        crossOriginIsolated.log('Not empty');
+    if (tds[trNumber][tdNumber].textContent !== '') {
+       console.log('Not empty');
+       
     } else {
         console.log('Empty');
         tds[trNumber][tdNumber].textContent = turn;
@@ -27,16 +25,16 @@ tds[trNumber].indexOf(event.target);
             tds[trNumber][1].textContent === turn &&
             tds[trNumber][2].textContent === turn 
          ) {
-            threeTd = ture;
+            threeTd = true;
          }
          if (
             tds[0][tdNumber].textContent === turn &&
             tds[1][tdNumber].textContent === turn &&
             tds[2][tdNumber].textContent === turn       
-         ){
+         ) {
             threeTd = true;
          }
-         if (trNumber - tdNumbere === 0) {
+         if (trNumber - tdNumber === 0) {
             if (
                 tds[0][0].textContent === turn &&
                 tds[1][1].textContent === turn &&
@@ -44,9 +42,8 @@ tds[trNumber].indexOf(event.target);
             ) {
                 threeTd = true;
                }
-         }
-         if (Math.abs(trNumber - tdNumber)===2)
-         {
+            }
+         if (Math.abs(trNumber - tdNumber)===2) {
             if (
                 tds[0][2].textContent === turn &&
                 tds[1][1].textContent === turn &&
@@ -55,8 +52,8 @@ tds[trNumber].indexOf(event.target);
                 threeTd = true ;
             }
       }
-      if (threTd) {
-        alert('Player' + turn + 'win!');
+      if (threeTd) {
+        alert('You' + '' + 'WIN !');
 
         turn = 'X';
         tds.forEach(function (trs) {
@@ -64,39 +61,40 @@ tds[trNumber].indexOf(event.target);
                 td.textContent = '';
             });
         });
-      }else {
-        if (turn === 'X') {
-            turn = 'O';
-        } else {
-            turn = 'X';
-        }
-      }
-  }
-};
 
-for (let i = 0; j <3; i++) {
+        } else {
+           if (turn === 'X') {
+               turn = 'O';
+             } else {
+            turn = 'X';
+             }
+        }
+     }
+ };
+
+for (let i = 0; i < 3; i++) {
     tds.push([]);
 };
+
 for (let j = 0; j < 3; j++) {
     tds[0].push(tr1.querySelectorAll('td').item(j));
     tds[1].push(tr2.querySelectorAll('td').item(j));
     tds[2].push(tr3.querySelectorAll('td').item(j));
 };
-for (let k = 0; k< 9; k++) {
-    const td =
-document.getElementsByTagName('td'). item(k);
-     td.addEventListener('click', marking);
+
+for (let k = 0; k < 9; k++) {
+    const td = document.getElementsByTagName('td'). item(k);
+    td.addEventListener('click', marking);
 };
-const resetBtn =
-document.getElementById('reset');
+const resetBtn = document.getElementById('reset');
 
 resetBtn.addEventListener('click', function() {
     turn = 'X';
     tds.forEach(function (trs) {
         trs.forEach(function (td) {
-        td.textContent = '';
+           td.textContent = '';
+         });
      });
-  });
-});
+ });
 
-console.log(tr, tds);
+console.log(trs, tds);
